b74da28b6a7d0af62afbe52e74630a0d63eb62c0
Andras Sagi <andras@pagem.hu>
2026-06-16 09:57:45 +0200

    bookingform

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/card";
 
 const psychologists = [
+  { id: "javorszky_eszter", name: "Jávorszky Eszter" },
   { id: "jeney-gabriella", name: "Jeney Gabriella" },
   { id: "jozsa-balazs", name: "Józsa Balázs" },
   { id: "lencses-anita", name: "Lencsés Anita" },
