e171484156f1a3b4c9cf362792667423bd004637
Andras Sagi <andras@pagem.hu>
2026-06-18 09:47:49 +0200

    map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -103,7 +103,7 @@ const Contact = () => {
                     <iframe 
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10746.976657371964!2d19.055845678624607!3d47.669966208453104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741d67aaad8a25f%3A0x40752374c8de13f6!2sSzentendre%2C%20B%C3%BCkk%C3%B6s%20part%2061%2C%202000!5e0!3m2!1sen!2shu!4v1781692675445!5m2!1sen!2shu" 
                       width="100%" 
-                      height="100%" 
+                      height="500" 
                       style={{border: 0}} 
                       allowFullScreen 
                       loading="lazy" 
